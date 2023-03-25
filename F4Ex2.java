296e0a899119db8249ed10758210e597c81012eb
Clayton Bazar <127749001+ClaytonBazar@users.noreply.github.com>
2023-03-25 15:36:34 +0200

    Update F4Ex2.java

--- F4Ex2.java
+++ F4Ex2.java
@@ -1,33 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package f4ex2;
+ import java.util.Scanner;
 
-/**
- *
- * @author Dell
- */
+ 
 public class F4Ex2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        ContaCorrente cc = new ContaCorrente();
-        ContaPoupanca cp = new ContaPoupanca();
-        ContaBancaria cb = new ContaBancaria();
-        cb.setDono("Nelson Bazar");
-        cc.setDono("Lurdes Jasse");
-        cb.setConta(213456984);
-        cc.setConta(875286358);
-        cp.setConta(835275257);
+public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+         
         
+         
+        System.out.println("Introduza o seu nome");
+        String dono = in.next();
+        System.out.println("Introduza o seu numero de conta");
+        int conta = in.nextInt();
+        ContaCorrente cc = new ContaCorrente(dono,conta);
+        System.out.println("Escolhe a operacao que deseja fazer ");
+        System.out.println("1.Depositar");
+        System.out.println("2.Levantar");
+        System.out.println("3.Dados da conta");
+        int opcao=in.nextInt();
+        switch (opcao){
+            case 1:
+                System.out.println("Introduza o valor que deseja depositar");
+                double valor = in.nextDouble();
+                System.out.println("A conta "+cc.getConta()+" depositou "+cc.depositar(valor));break;
         
-        System.out.println("Saldo: "+cc.getSaldo());
-        System.out.println("A conta "+cc.getConta()+" de "+cc.getDono()+ " depositou "+cc.depositar(2500)+" Levantou "+cc.sacar(4000)+". A taxa de operacao foi de "+cc.taxaDeOperacao()+" e o seu saldo e de "+cc.getSaldo());
-        System.out.println("A conta " + cp.getConta()+" A "+cp.Limite(3000f)+" Tipo de conta: "+cp.getTipo());
-        System.out.println(cb.mostrarDados());
+            case 2:
+                System.out.println("Introduza o valor que deseja Levantar");
+                valor = in.nextDouble();
+                System.out.println("A conta "+cc.getConta()+" levantou "+cc.sacar(valor)+"O saldo atual e de "+cc.getSaldo());break;
+           
+            case 3: 
+                System.out.println(cc.mostrarDados());
+        }
+           
+            
     }
+}    
     
-}
